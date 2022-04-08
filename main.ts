e4a0aed030b073fb3afd1ec28c3e177255cf6533
mrateroech <93262453+mrateroech@users.noreply.github.com>
2022-04-08 14:15:33 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
-DFRobotMaqueenPlus.I2CInit()
-basic.forever(function () {
-    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.CYAN)
-    basic.pause(1000)
-    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.RED)
-    basic.pause(1000)
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.CYAN)
+        basic.pause(100)
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.OFF)
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.RED)
+        basic.pause(100)
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.OFF)
+    }
 })
+DFRobotMaqueenPlus.I2CInit()
